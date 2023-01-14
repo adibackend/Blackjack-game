a623318e089361140d4a28dffeb88837b4d64933
adi <111768380+deviiiiiii@users.noreply.github.com>
2023-01-14 23:27:06 +0530

    Update yo.js

--- yo.js
+++ yo.js
@@ -4,8 +4,3 @@ varr.textContent=""
 for(let i=0;i<sen.length;i++){
         varr.textContent=varr.textContent+sen[i]+" ";
 }
-// varr.textContent=sen[3]
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-    
-// }
