a830d694282de83cfd66b5e393d07288fe612c5f
adi <temp>
2023-01-14 17:54:57 +0000

    first black jack project

---
+++ javascript/index.js
@@ -0,0 +1,58 @@
+// let card[0]=7;
+// card[1]=13;
+let i=0;
+let isalive=false
+let message="Do you want to draw a new card?"
+let firstcard=getrandomnumber()
+let secondcard=getrandomnumber()
+// total=card[0]+card[1];
+let card=[firstcard,secondcard];
+total=firstcard+secondcard
+function startgame(){
+    
+    isalive=true
+    let sum=document.getElementById("sum");
+    let messageU=document.getElementById("messeage");
+    let cards=document.getElementById("cards");
+    messageU.textContent=message;
+    if (total==21) {
+        messageU.textContent="You Got Blackjack";
+    }
+    else if(total>21){
+        messageU.textContent="You Loose";
+    }
+    else{
+        messageU.textContent="Want more card?";
+    }
+    cards.textContent="Cards : "
+    for (i= 0; i < card.length; i++) {
+         cards.textContent =  cards.textContent+card[i] + " ";
+         
+    }
+    // cards.textContent="";
+    sum.textContent= "Sum : "+ total;
+}
+function drawcard() {
+    if (isalive==false || total>=21) {
+        // console.error("sorry")
+        return;
+    }
+   let card3=getrandomnumber();
+   card.push(card3);
+   total +=card3;
+   startgame();
+
+}
+function getrandomnumber(){
+    let ran=Math.floor(Math.random()*13)+1
+    if(ran==1){
+        return 11;
+    }
+    else if(ran>10){
+        return 10;
+    }
+    else{
+        return ran; 
+
+    }
+}
---
+++ yo.js
@@ -0,0 +1,11 @@
+let varr=document.getElementById("para")
+let sen=["this","is","my","sentence"]
+varr.textContent=""
+for(let i=0;i<sen.length;i++){
+        varr.textContent=varr.textContent+sen[i]+" ";
+}
+// varr.textContent=sen[3]
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
