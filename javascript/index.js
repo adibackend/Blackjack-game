6bc5bc2a35c49c6444aff862fa4848035aa4206b
adi <temp>
2023-01-14 20:06:06 +0000

    removed unwated files and modified index and js file

--- javascript/index.js
+++ javascript/index.js
@@ -5,15 +5,16 @@ let isalive=false
 let message="Do you want to draw a new card?"
 let firstcard=getrandomnumber()
 let secondcard=getrandomnumber()
+let resetBtn=document.getElementById("reset-btn")
 // total=card[0]+card[1];
 let card=[firstcard,secondcard];
-total=firstcard+secondcard
+let total=firstcard+secondcard
+let sum=document.getElementById("sum");
+let messageU=document.getElementById("messeage");
+let cards=document.getElementById("cards");
 function startgame(){
     
     isalive=true
-    let sum=document.getElementById("sum");
-    let messageU=document.getElementById("messeage");
-    let cards=document.getElementById("cards");
     messageU.textContent=message;
     if (total==21) {
         messageU.textContent="You Got Blackjack";
@@ -27,7 +28,6 @@ function startgame(){
     cards.textContent="Cards : "
     for (i= 0; i < card.length; i++) {
          cards.textContent =  cards.textContent+card[i] + " ";
-         
     }
     // cards.textContent="";
     sum.textContent= "Sum : "+ total;
@@ -44,6 +44,7 @@ function drawcard() {
 
 }
 function getrandomnumber(){
+    
     let ran=Math.floor(Math.random()*13)+1
     if(ran==1){
         return 11;
@@ -56,3 +57,14 @@ function getrandomnumber(){
 
     }
 }
+resetBtn.addEventListener("click",function(){
+        total=0
+        sum.textContent="Sum : "
+        card=[];
+        cards.textContent="Cards : "
+        startgame()
+})
+// function banda(){
+//     // isalive=true
+    
+// }
--- yo.js
+++
@@ -1,6 +0,0 @@
-let varr=document.getElementById("para")
-let sen=["this","is","my","sentence"]
-varr.textContent=""
-for(let i=0;i<sen.length;i++){
-        varr.textContent=varr.textContent+sen[i]+" ";
-}
